2ef1ac077cd5f489690649b289a755863715691b
sorted-hd <hdubey71@gmail.com>
2022-05-01 23:40:02 +0530

    Added Screens Layout

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,23 @@
+import { StyleSheet, TextInput, View } from 'react-native';
+
+import PrimaryButton from '../components/PrimaryButton';
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    padding: 16,
+    marginTop: 100,
+  },
+});
